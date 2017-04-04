ea969d5ccf990292455b18a936c3cc0d36c902d6
Bartek <bartek9393@poczta.onet.pl>
2017-04-04 23:10:03 +0100

    Removed previous testing code

--- app/src/main/java/com/example/bartek/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/bartek/myfirstapp/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.bartek.myfirstapp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
-import android.widget.EditText;
 import android.widget.TextView;
 
-import static android.graphics.Color.*;
-import static android.graphics.Typeface.*;
+import static android.graphics.Color.BLUE;
+import static android.graphics.Color.CYAN;
+import static android.graphics.Color.GRAY;
+import static android.graphics.Color.GREEN;
+import static android.graphics.Color.MAGENTA;
+import static android.graphics.Color.RED;
+import static android.graphics.Color.YELLOW;
+import static android.graphics.Typeface.BOLD;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.bartek.myfirstapp.MESSAGE";
@@ -20,25 +24,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
-
-    public void sumNumbers(View view) {
-        EditText e1 = (EditText) findViewById(R.id.number1);
-        EditText e2 = (EditText) findViewById(R.id.number2);
-        TextView textView = (TextView) findViewById(R.id.result);
-        int first = Integer.parseInt(e1.getText().toString());
-        int second = Integer.parseInt(e2.getText().toString());
-        int sum = first + second;
-        String result = Integer.toString(sum);
-        textView.setText(result);
     }
 
     public void test(View view) {
