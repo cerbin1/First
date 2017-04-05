39fb21833799c57f367cb2b5a1c23c21481c0ea1
Bartek <bartek9393@poczta.onet.pl>
2017-04-05 03:02:46 +0100

    bug fix

--- app/src/main/java/com/example/bartek/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/bartek/myfirstapp/MainActivity.java
@@ -37,61 +37,61 @@ public class MainActivity extends AppCompatActivity {
         String count = Integer.toString(counter);
         textView.setText(count);
 
-        if (counter == 1) {
+        if (counter >= 1) {
             changeDisplayOf(R.id.achievement1, Color.parseColor("#52be80"));
         }
-        if (counter == 5) {
+        if (counter >= 5) {
             changeDisplayOf(R.id.achievement5, Color.parseColor("#27ae60"));
         }
-        if (counter == 10) {
+        if (counter >= 10) {
             changeDisplayOf(R.id.achievement10, Color.parseColor("#229954"));
         }
-        if (counter == 20) {
+        if (counter >= 20) {
             changeDisplayOf(R.id.achievement20, Color.parseColor("#1e8449"));
         }
-        if (counter == 50) {
+        if (counter >= 50) {
             changeDisplayOf(R.id.achievement50, Color.parseColor("#196f3d"));
         }
-        if (counter == 75) {
+        if (counter >= 75) {
             changeDisplayOf(R.id.achievement75, Color.parseColor("#145a32"));
         }
-        if (counter == 100) {
+        if (counter >= 100) {
             changeDisplayOf(R.id.achievement100, Color.parseColor("#5dade2"));
         }
-        if (counter == 200) {
+        if (counter >= 200) {
             changeDisplayOf(R.id.achievement200, Color.parseColor("#3498db"));
         }
-        if (counter == 300) {
+        if (counter >= 300) {
             changeDisplayOf(R.id.achievement300, Color.parseColor("#2e86c1"));
         }
-        if (counter == 400) {
+        if (counter >= 400) {
             changeDisplayOf(R.id.achievement400, Color.parseColor("#2874a6"));
         }
-        if (counter == 500) {
+        if (counter >= 500) {
             changeDisplayOf(R.id.achievement500, Color.parseColor("#21618c"));
         }
-        if (counter == 1000) {
+        if (counter >= 1000) {
             changeDisplayOf(R.id.achievement1000, Color.parseColor("#1b4f72"));
         }
-        if (counter == 2000) {
+        if (counter >= 2000) {
             changeDisplayOf(R.id.achievement2000, RED);
         }
-        if (counter == 3000) {
+        if (counter >= 3000) {
             changeDisplayOf(R.id.achievement3000, Color.parseColor("#FFA500"));
         }
-        if (counter == 4000) {
+        if (counter >= 4000) {
             changeDisplayOf(R.id.achievement4000, YELLOW);
         }
-        if (counter == 5000) {
+        if (counter >= 5000) {
             changeDisplayOf(R.id.achievement5000, GREEN);
         }
-        if (counter == 6000) {
+        if (counter >= 6000) {
             changeDisplayOf(R.id.achievement6000, BLUE);
         }
-        if (counter == 7000) {
+        if (counter >= 7000) {
             changeDisplayOf(R.id.achievement7000, Color.parseColor("#9B30FF"));
         }
-        if (counter == 10000) {
+        if (counter >= 10000) {
             changeDisplayOf(R.id.achievement10000, CYAN);
         }
     }
